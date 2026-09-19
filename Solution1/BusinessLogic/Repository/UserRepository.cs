c6be6bc4f5a1cf845d7e3d26c74fa9b007110c41
Vince-Jhaylourd <vincetibayan13th@gmail.com>
2026-09-19 21:28:53 +0700

    Set up login architecture

---
+++ Solution1/BusinessLogic/Repository/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace BusinessLogic.Repository
+{
+    internal class UserRepository
+    {
+        public User? Login(string username, string password)
+        {
+            return null;
+        }
+    }
+}  
---
+++ Solution1/DB/StoredProc/spLogin.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE spLogin
+	@Username NVARCHAR(50),
+	@PasswordHash NVARCHAR(255)
+	AS 
+BEGIN
+	SELECT 
+		UserId,
+		Username
+	FROM [User]
+	WHERE Username = @Username
+	AND Password = @Password
+END;
