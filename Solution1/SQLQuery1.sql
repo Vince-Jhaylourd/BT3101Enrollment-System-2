e3ed18c78f52bf7d6977166532090905633a8163
Vince-Jhaylourd <vincetibayan13th@gmail.com>
2026-09-20 02:46:08 +0700

    Left files

---
+++ Solution1/SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿USE EnrollmentSystem;
+
+CREATE TABLE [dbo].[User]
+(
+	UserId INT IDENTITY(1,1) PRIMARY KEY,
+	Username NVARCHAR(50) NOT NULL,
+	Password NVARCHAR(255) NOT NULL,
+	);
+
+	INSERT INTO [dbo].[User] (Username, Password) VALUES
+	('admin', '12345');
+	
---
+++ Solution1/SQLQuery2.sql
@@ -0,0 +1,19 @@
+﻿USE EnrollmentSystem;
+GO
+
+CREATE PROCEDURE dbo.spLogin
+	@Username NVARCHAR(50),
+	@Password NVARCHAR(255)
+	AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT 
+		UserId,
+		Username
+	FROM [dbo].[User]
+	WHERE
+		Username = @Username 
+		AND Password = @Password;
+END;
+GO
