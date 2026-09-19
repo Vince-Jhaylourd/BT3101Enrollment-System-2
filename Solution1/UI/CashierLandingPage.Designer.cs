1ce6caab5466284fe1be095b3c68d423c3d775cb
angelynjoyteologo30@gmail.com <angelynjoyteologo30@gmail.com>
2026-09-19 22:33:50 +0800

    Move  Process payment button

--- Solution1/UI/CashierLandingPage.Designer.cs
+++ Solution1/UI/CashierLandingPage.Designer.cs
@@ -83,7 +83,7 @@
             // btnProcessPayment
             // 
             this.btnProcessPayment.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnProcessPayment.Location = new System.Drawing.Point(405, 92);
+            this.btnProcessPayment.Location = new System.Drawing.Point(418, 92);
             this.btnProcessPayment.Name = "btnProcessPayment";
             this.btnProcessPayment.Size = new System.Drawing.Size(180, 109);
             this.btnProcessPayment.TabIndex = 5;
